84323e89f56ae02f3d4b1d5c8d05d3caef16b3da
0xABDee <111497238+0xABDee@users.noreply.github.com>
2022-08-22 15:46:15 +0500

    navich
    navich

--- navich.sol
+++ navich.sol
@@ -4,7 +4,9 @@ pragma solidity 0.8.7;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract Navich is ERC20 {
-    constructor(uint amount) ERC20("Navich", "NAVICH") {
-        _mint(msg.sender, amount);
+    constructor() ERC20("Navich", "NAVICH") {}
+
+    function buyNavich(uint256 tokenAmount) public {
+        _mint(msg.sender, tokenAmount * 10**18);
     }
 }
